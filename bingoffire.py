d398fb83f30fa6072e541966fd04acb807dfe6f2
Alex Turek <alex.turek@gmail.com>
2014-02-22 09:33:06 -0800

    Consolidating html routing. Removing some cargo-cult crap

--- bingoffire.py
+++ bingoffire.py
@@ -19,7 +19,7 @@ log_to_stdout.setFormatter(LOG_FORMAT)
 app = flask.Flask(__name__)
 app.logger.addHandler(log_to_stdout)
 
-_PAGE_CACHE = 5 * 60
+_PAGE_CACHE = 10
 _ASSET_CACHE = 5 * 60 * 60
 
 
@@ -39,58 +39,45 @@ def index():
     return _send_file('', 'index.html', cache_timeout=_PAGE_CACHE)
 
 
-@app.route('/contact')
-def catering():
-    return _send_file('', 'contact.html', cache_timeout=_PAGE_CACHE)
+paths_to_page = {
+    'contact': 'contact.html',
+    'gallery': 'gallery.html',
+    'about': 'about.html',
+    'menu_en': 'menu_en.html',
+    'menu_cn': 'menu_cn.html',
+}
 
 
-@app.route('/gallery')
-def gallery():
-    return _send_file('', 'gallery.html', cache_timeout=_PAGE_CACHE)
-
-
-@app.route('/about')
-def about():
-    return _send_file('', 'about.html', cache_timeout=_PAGE_CACHE)
-
-
-@app.route('/menu_en')
-def menu_en():
-    return _send_file('', 'menu_en.html', cache_timeout=_PAGE_CACHE)
-
-
-@app.route('/menu_cn')
-def menu_cn():
-    return _send_file('', 'menu_cn.html', cache_timeout=_PAGE_CACHE)
+@app.route('/<path:page>')
+def html_pages():
+    if path in paths_to_page:
+        return _send_file('', paths_to_page[page], cache_timeout=_PAGE_CACHE)
+    else:
+        return _send_file('', '404.html', cache_timeout=_PAGE_CACHE)
 
 
 @app.route('/js/<path:filename>')
 def deliver_js(filename):
-    flask.g.is_static = True
     return _send_file('/js', filename, cache_timeout=_ASSET_CACHE)
 
 
 @app.route('/css/<path:filename>')
 def deliver_css(filename):
-    flask.g.is_static = True
     return _send_file('/css', filename, cache_timeout=_ASSET_CACHE)
 
 
 @app.route('/img/<path:filename>')
 def deliver_img(filename):
-    flask.g.is_static = True
     return _send_file('/img', filename, cache_timeout=_ASSET_CACHE)
 
 
 @app.route('/img/<filename>.png')
 def png_files(filename):
-    flask.g.is_static = True
     return _send_file('/img', filename + '.png', mimetype='image/png', cache_timeout=_ASSET_CACHE)
 
 
 @app.route('/favicon.ico')
 def favicon():
-    flask.g.is_static = True
     return _send_file('/img', 'favicon.ico', mimetype='image/vnd.microsoft.icon',
         as_attachment=False, cache_timeout=_ASSET_CACHE)
 
